6cc7817a4fece1f7762c80c6da249e4f52e85b6d
Vitor de Paula Gomes <112875256+VitorPgms@users.noreply.github.com>
2023-04-13 17:53:46 -0300

    java att1

---
+++ aulas/JavaScript/ex002/index.js
@@ -0,0 +1,11 @@
+let nome = "Vitor"
+
+console.log (nome)
+console.error (nome)
+console.warn (nome)
+
+//window.alert (nome)
+//nome = window.prompt ("Insira o seu nome")
+//et confirmacao = window.confirm ("Confirma essa açao?")
+
+//document Não vamos estudar por enquanto mas e bastante utilizada.
