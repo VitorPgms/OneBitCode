e70eebe5e61f19cf007a636e9352512e07617e6d
Vitor de Paula Gomes <112875256+VitorPgms@users.noreply.github.com>
2023-04-17 18:03:26 -0300

    java if e else

---
+++ aulas/JavaScript/ex003/index.js
@@ -0,0 +1,26 @@
+const idade = prompt ("Informe a sua idade")
+
+
+/* if (idade > 18) {
+    alert("Você ja pode ser preso")
+}
+
+else {
+    alert ("Voce ainda ta safe")
+
+    if (idade < 12) {
+        alert ("Voce ta muito novo parça")
+    }
+} */
+
+if (idade > 18) {
+    alert ("Voce e maior de idade")
+}
+
+else if (idade > 12) {
+    alert ("Voce e menor de idade")
+}
+
+else if (idade > 4) {
+    alert ("Voce e criança")
+}
